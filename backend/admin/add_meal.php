8275275742affc1eba7f20c4ac7b56d9ba614533
MUNYOH JOEL TAMON <190579309+MUNYOH-JOEL-TAMON@users.noreply.github.com>
2026-01-20 23:36:52 -0800

    fixing some bugs

--- backend/admin/add_meal.php
+++ backend/admin/add_meal.php
@@ -15,11 +15,7 @@ if (!isLoggedIn()) {
     exit();
 }
 
-if (!isAdmin()) {
-    http_response_code(403);
-    echo json_encode(["success" => false, "message" => "Forbidden. Admin access required."]);
-    exit();
-}
+// TODO: Add Role Check here
 
 $database = new Database();
 $db = $database->getConnection();
